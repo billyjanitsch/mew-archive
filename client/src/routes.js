45cf2f6f716a6c67ab8e50fa220f5e9208bb8fc2
Billy Janitsch <billy@kensho.com>
2016-05-28 17:24:56 -0400

    add Artists page

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,8 +1,9 @@
-import {Route} from 'react-router'
+import {Route, IndexRedirect} from 'react-router'
 import {App, Artists, Artist, NotFound} from './components'
 
 export default (
   <Route path='/' component={App}>
+    <IndexRedirect to='artists' />
     <Route path='artists' component={Artists}>
       <Route path=':artist' component={Artist} />
     </Route>
