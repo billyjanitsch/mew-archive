915fa75ccb57f27a70a1c166242d9143996895b4
Billy Janitsch <billy@kensho.com>
2016-05-29 20:43:22 -0400

    remove insert util

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,6 +1,5 @@
 export bandname from './bandname'
 export ensureArray from './ensureArray'
 export getStore from './getStore'
-export insert from './insert'
 export invariant from './invariant'
 export Provider from './Provider'
--- client/src/utils/insert.js
+++
@@ -1,9 +0,0 @@
-import ensureArray from './ensureArray'
-
-const insert = (arr, item, position) => [
-  ...arr.slice(0, position + 1),
-  ...ensureArray(item),
-  ...arr.slice(position + 1),
-]
-
-export default insert
